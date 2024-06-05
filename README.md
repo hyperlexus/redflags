4deb58fea0e567d1dcdec6a81653ec91e1e054ad
HyperLexus <alexberg1444@gmail.com>
2024-06-05 16:51:37 +0200

    setting up and first UI

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # redflags
-flag trainer
+flag trainer with different difficulty modes.
+
+## easy:
+three options from the entire world
+
+## medium:
+four options from the same continent
+
+## hard:
+input field for the country (you have to know it)
+
+## insane:
+just a map where you have to click where the corresponding country is
+
---
+++ script.js
@@ -0,0 +1,17 @@
+// https://gist.github.com/keeguon/2310008
+// [
+//     {
+//         "name": "Germany",
+//         "countryCode": "de",
+//         "region": "Europe",
+//         "coordinates": [51.1657, 10.4515]
+//     },
+//     // ... more countries
+// ]
+
+// url format:
+// <img
+//   src="https://flagcdn.com/160x120/ua.png"
+//   width="160"
+//   height="120"
+//   alt="Ukraine">
